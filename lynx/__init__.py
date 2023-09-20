e5c3095436a06f938186feef94daf122d9a20b84
mochen.bmc <mochen.bmc@antgroup.com>
2023-09-20 14:51:44 +0800

    update

--- lynx/__init__.py
+++ lynx/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# 
+#
 #   Lynx is a deep learning automatic compilation system primarily aimed at
 # computation optimization and automatic distributed optimization. It is
 # built upon the open-source foundations of TorchXLA and OpenXLA/LLVM.
-# 
+#
 #   It is primarily divided into two components: a compiler and a runtime.
 # The compiler is responsible for lowering PyTorch's computation graph into
 # an intermediate representation known as StableHLO and performing various
@@ -12,4 +12,4 @@
 # using LLVM. The runtime component primarily handles the scheduling of
 # the compiled SPMD (Single Program, Multiple Data) programs to be executed
 # on computing nodes.
-# 
+#
